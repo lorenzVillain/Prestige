b2ae201dbba2d0caf0ce539f416c32defdf8d677
Lorenz Villain <dragondistribution@gmail.com>
2015-12-04 11:09:26 +0100

    root fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root 'products#index'
+  root '/home'
   resources :products
 
   get "/home", to: "pages#index"
